298f67c86ec989b4e6cca55d9f1a0edbe33f9dcd
danielc <chinonso48@yahoo.com>
2025-09-20 13:26:44 +0100

    fix(terraform): correct outputs to use count index

--- modules/baseline/outputs.tf
+++ modules/baseline/outputs.tf
@@ -1,4 +1,15 @@
+output "kms_ebs_key_arn_out" {
+  description = "KMS ARN for EBS encryption (created or provided)"
+  value       = try(aws_kms_key.ebs[0].arn, var.kms_ebs_key_arn, null)
+}
 
-output "kms_ebs_key_arn_out" { value = try(aws_kms_key.ebs.arn, var.kms_ebs_key_arn) }
-output "kms_logs_key_arn_out" { value = try(aws_kms_key.logs.arn, var.kms_logs_key_arn) }
-output "kms_data_key_arn_out" { value = try(aws_kms_key.data.arn, var.kms_data_key_arn) }
+output "kms_logs_key_arn_out" {
+  description = "KMS ARN for CloudWatch Logs encryption (created or provided)"
+  value       = try(aws_kms_key.logs[0].arn, var.kms_logs_key_arn, null)
+}
+
+output "kms_data_key_arn_out" {
+  description = "KMS ARN for data encryption (created or provided)"
+  value       = try(aws_kms_key.data[0].arn, var.kms_data_key_arn, null)
+}
+
