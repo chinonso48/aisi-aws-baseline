c1f5a93d19206a514acd2306a44508a103d7c03a
chinonso48 <166803338+chinonso48@users.noreply.github.com>
2025-09-20 12:25:10 +0100

    Update s3_encryption.tf

--- modules/baseline/s3_encryption.tf
+++ modules/baseline/s3_encryption.tf
@@ -1,11 +1,12 @@
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "defaults" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   for_each = toset(var.s3_bucket_names)
-  bucket   = each.value
+
+  bucket = each.value
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = coalesce(try(aws_kms_key.data.0.arn, null), var.kms_data_key_arn, try(aws_kms_key.logs.0.arn, null))
+      kms_master_key_id = var.kms_data_key_arn != "" ? var.kms_data_key_arn : null
     }
   }
 }
